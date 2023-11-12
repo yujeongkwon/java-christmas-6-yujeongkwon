091d9f9d314778ba70afc1b44d4fe53c7004cac0
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-13 02:59:20 +0900

    feat(InputView) : 방문 날짜 입력 받기

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String readVisitDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return Console.readLine();
+    }
+}
