869be99f0bbbbaa1294d847f4de2a690a594dbd1
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-13 03:00:40 +0900

    feat(InputView) : 메뉴와 개수 입력 받기

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,8 +3,14 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
     public static String readVisitDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return Console.readLine();
+    }
+
+    public static String readOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         return Console.readLine();
     }
 }
