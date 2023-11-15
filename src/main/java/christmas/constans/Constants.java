ce5db0626dd6c72c43ba01ed4108bb6a1ad479eb
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-15 19:59:39 +0900

    feat(OutputView) : 이벤트 해택 미리보기 안내 출력

--- src/main/java/christmas/constans/Constants.java
+++ src/main/java/christmas/constans/Constants.java
@@ -3,4 +3,5 @@ package christmas.constans;
 public final  class Constants {
     public static final int MIN_QUANTITY = 1;
     public static final int MAX_QUANTITY = 20;
+    public static final int EVENT_MONTH = 12;
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,9 @@
 package christmas.view;
 
+import java.time.LocalDate;
+
+import static christmas.constans.Constants.EVENT_MONTH;
+
 public class OutputView {
 
     public static void printException(Exception exception) {
@@ -8,5 +12,10 @@ public class OutputView {
 
     public static void printWelcomeMessage() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public static void printEventPreview(LocalDate visitDate) {
+        System.out.println(String.format("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기! \n",
+                            EVENT_MONTH, visitDate.getDayOfMonth()));
     }
 }
