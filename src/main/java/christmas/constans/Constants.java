659e579e912f2617c6841bdd77bd0e155c81fdec
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-15 23:19:28 +0900

    fix() : 날짜 예외 오류 수정 및 상수화

--- src/main/java/christmas/constans/Constants.java
+++ src/main/java/christmas/constans/Constants.java
@@ -3,5 +3,6 @@ package christmas.constans;
 public final class Constants {
     public static final int MIN_QUANTITY = 1;
     public static final int MAX_QUANTITY = 20;
+    public static final int EVENT_YEAR = 2023;
     public static final int EVENT_MONTH = 12;
 }
--- src/main/java/christmas/validation/VisitDateValidator.java
+++ src/main/java/christmas/validation/VisitDateValidator.java
@@ -3,18 +3,30 @@ package christmas.validation;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 
+import static christmas.constans.Constants.EVENT_MONTH;
+import static christmas.constans.Constants.EVENT_YEAR;
 import static christmas.constans.ExceptionMessage.INVALID_DATE;
 
 public class VisitDateValidator {
+
     public static LocalDate readVisitDate() {
         try {
             int input = convertVisitDateInputToInt(InputView.readVisitDate());
-            return LocalDate.of(2023, 12, input);
+            return createLocalDate(EVENT_YEAR, EVENT_MONTH, input);
         } catch (IllegalArgumentException e) {
             OutputView.printException(e);
             return readVisitDate();
+        }
+    }
+
+    private static LocalDate createLocalDate(int year, int month, int dayOfMonth) {
+        try {
+            return LocalDate.of(year, month, dayOfMonth);
+        } catch (DateTimeException e) {
+            throw new IllegalArgumentException(INVALID_DATE.get());
         }
     }
 
@@ -25,5 +37,4 @@ public class VisitDateValidator {
             throw new IllegalArgumentException(INVALID_DATE.get());
         }
     }
-
 }
