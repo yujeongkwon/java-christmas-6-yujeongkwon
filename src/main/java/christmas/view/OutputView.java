836306f2725d9dd92726602d868c05235b416dc6
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-15 19:50:35 +0900

    feat(OutputView) :12월 이벤트 플래너 시작 안내 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public static void printException(Exception exception) {
         System.out.println(exception.getMessage());
     }
+
+    public static void printWelcomeMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
 }
