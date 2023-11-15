c7dfd8a5c2c8aac993fb0af6d39de1f2839933ba
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-15 19:11:11 +0900

    feat(BenefitCalculator) : 할인 후 예상 결제 금액

--- src/main/java/christmas/domain/BenefitCalculator.java
+++ src/main/java/christmas/domain/BenefitCalculator.java
@@ -5,11 +5,12 @@ import java.util.Optional;
 
 public class BenefitCalculator {
     public static int calculateTotalBenefits(Map<String, Integer> discountedInfo, Optional<GiftMenu> giftMenu) {
-        discountedInfo.forEach((strategy, amount) ->
-                System.out.println(strategy + ": -" + String.format("%,d원", amount)));
         int discountBenefits = discountedInfo.values().stream().mapToInt(Integer::intValue).sum();
         int giftBenefits = giftMenu.map(GiftMenu::getPrice).orElse(0);
         return discountBenefits + giftBenefits;
     }
 
+    public static int calculateDiscountedAmount(int totalPrice, int totalDiscounted){
+        return totalPrice - totalDiscounted;
+    }
 }
